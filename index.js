5290bd30c6c361c42e714ccde2976ed755e9b57c
allan6757 <allanoomaina@gmail.com>
2025-06-25 14:59:00 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            // Append the new id to the DOM
+            const body = document.querySelector('body');
+            body.innerHTML += `<p>${data.id}</p>`;
+        })
+        .catch(error => {
+            // Append the error message to the DOM
+            const body = document.querySelector('body');
+            body.innerHTML += `<p>${error.message}</p>`;
+        });
+}
